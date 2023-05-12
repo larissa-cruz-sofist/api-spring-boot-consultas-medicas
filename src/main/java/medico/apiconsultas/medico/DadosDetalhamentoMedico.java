2ec104b992b58891896ace3e8753d22c488e7a3a
Larissa Cruz <larissa.cruz@sofist.co>
2023-05-12 16:57:44 -0300

    alterando dados detalhamento medico e dados listagem medico

--- src/main/java/medico/apiconsultas/medico/DadosDetalhamentoMedico.java
+++ src/main/java/medico/apiconsultas/medico/DadosDetalhamentoMedico.java
@@ -2,10 +2,10 @@ package medico.apiconsultas.medico;
 
 import medico.apiconsultas.endereco.*;
 
-public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco) {
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone, Especialidade especialidade, Endereco endereco, Boolean ativo) {
 
 	   public DadosDetalhamentoMedico(Medico medico) {
-	        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco());
+	        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(), medico.getEndereco(), medico.getAtivo());
 	    }
 	
 }
--- src/main/java/medico/apiconsultas/medico/DadosListagemMedico.java
+++ src/main/java/medico/apiconsultas/medico/DadosListagemMedico.java
@@ -1,9 +1,9 @@
 package medico.apiconsultas.medico;
 
-public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade, Boolean ativo) {
 
 	public DadosListagemMedico(Medico medico) {
-		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+		this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade(), medico.getAtivo());
 	}
 	
 }
