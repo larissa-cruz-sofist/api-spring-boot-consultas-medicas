2ca9b3db77e0a940c88d380f49452d64b9efa3ce
Larissa Cruz <larissa.cruz@sofist.co>
2023-05-15 11:34:35 -0300

    alterado DadosListagem e DadosDetalhemento Paciente

--- src/main/java/medico/apiconsultas/paciente/DadosDetalhamentoPaciente.java
+++ src/main/java/medico/apiconsultas/paciente/DadosDetalhamentoPaciente.java
@@ -2,10 +2,10 @@ package medico.apiconsultas.paciente;
 
 import medico.apiconsultas.endereco.*;
 
-public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco) {
+public record DadosDetalhamentoPaciente(Long id, String nome, String email, String cpf, String telefone, Endereco endereco, Boolean ativo) {
 	
 	 public DadosDetalhamentoPaciente(Paciente paciente) {
-	        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco());
+	        this(paciente.getId(), paciente.getNome(), paciente.getEmail(), paciente.getCpf(), paciente.getTelefone(), paciente.getEndereco(), paciente.getAtivo());
 	    }
 
 }
