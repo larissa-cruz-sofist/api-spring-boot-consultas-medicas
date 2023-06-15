71b237b69b13086c92e0db69c89edee6247558cd
Larissa Cruz <larissa.cruz@sofist.co>
2023-06-15 16:41:55 -0300

    criando testes em Paciente Repository e Medico Repository

--- src/test/java/medico/apiconsultas/medico/MedicoTest.java
+++ src/test/java/medico/apiconsultas/medico/MedicoTest.java
@@ -2,6 +2,8 @@ package medico.apiconsultas.medico;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+
 import medico.apiconsultas.endereco.DadosEndereco;
 import medico.apiconsultas.endereco.Endereco;
 
@@ -10,19 +12,53 @@ import org.junit.jupiter.api.Test;
 class MedicoTest {
 
 	@Test
-	void testAtualizarInformacoesMedico() {
+	@DisplayName("Deve alterar os dados do médico, nome, telefone e endereco")
+	void testAtualizarInformacoesMedico_cenario1() {
 		
 		var especialidade = Especialidade.CARDIOLOGIA;
 		var endereco = new Endereco("rua baraldi", "santa paula", "12300412", "20", "bloco D", "Sao Paulo", "SP");
 		
 		Medico medico = new Medico(null, "Marcos", "marcoslinhares@vold.med", "801208", "892083001", especialidade, endereco, true);
 		
-		DadosAtualizacaoMedico dadosAtualizacaoMedico = new DadosAtualizacaoMedico(null, "Marcos Alberto", "892083221", new DadosEndereco("rua baraldi", "santa paula", "12300412", "Sao Paulo", "SP", "bloco D", "20"));
+		DadosAtualizacaoMedico dadosAtualizacaoMedico = new DadosAtualizacaoMedico(null, "Marcos Alberto", "892083221", new DadosEndereco("rua paes de barros", "vila mariana", "03125789", "Minas Gerais", "MG", "bloco H", "33"));
 		
 		medico.atualizarInformacoes(dadosAtualizacaoMedico);
 		
 		assertEquals("Marcos Alberto", medico.getNome());
 		assertEquals("892083221", medico.getTelefone());
+		assertEquals("rua paes de barros", medico.getEndereco().getLogradouro());
+		assertEquals("vila mariana", medico.getEndereco().getBairro());
+		assertEquals("03125789", medico.getEndereco().getCep());
+		assertEquals("Minas Gerais", medico.getEndereco().getCidade());
+		assertEquals("MG", medico.getEndereco().getUf());
+		assertEquals("bloco H", medico.getEndereco().getComplemento());
+		assertEquals("33", medico.getEndereco().getNumero());
+		
+		
+	}
+	
+	@Test
+	@DisplayName("Não deve alterar os dados do médico, nome, telefone e endereco")
+	void testAtualizarInformacoesMedico_cenario2() {
+		
+		var especialidade = Especialidade.DERMATOLOGIA;
+		var endereco = new Endereco("rua paraiba", "bela vista", "02578456", "26", "bloco F", "Bahia", "BA");
+		
+		Medico medico = new Medico(null, "Cesar", "cesar@vold.med", "805558", "204083001", especialidade, endereco, true);
+		
+		DadosAtualizacaoMedico dadosAtualizacaoMedico = new DadosAtualizacaoMedico(null, "Cesar", "204083001", new DadosEndereco("rua paraiba", "bela vista", "02578456", "Bahia", "BA", "bloco F", "26"));
+		
+		medico.atualizarInformacoes(dadosAtualizacaoMedico);
+		
+		assertEquals("Cesar", medico.getNome());
+		assertEquals("204083001", medico.getTelefone());
+		assertEquals("rua paraiba", medico.getEndereco().getLogradouro());
+		assertEquals("bela vista", medico.getEndereco().getBairro());
+		assertEquals("02578456", medico.getEndereco().getCep());
+		assertEquals("Bahia", medico.getEndereco().getCidade());
+		assertEquals("BA", medico.getEndereco().getUf());
+		assertEquals("bloco F", medico.getEndereco().getComplemento());
+		assertEquals("26", medico.getEndereco().getNumero());
 		
 		
 	}
--- src/test/java/medico/apiconsultas/paciente/PacienteTest.java
+++ src/test/java/medico/apiconsultas/paciente/PacienteTest.java
@@ -2,6 +2,7 @@ package medico.apiconsultas.paciente;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import medico.apiconsultas.endereco.DadosEndereco;
@@ -10,19 +11,50 @@ import medico.apiconsultas.endereco.Endereco;
 class PacienteTest {
 
 	@Test
-	void testAtualizarInformacoesPaciente() {
+	@DisplayName("Deve alterar os dados do paciente, nome, telefone e endereco")
+	void testAtualizarInformacoesPaciente_cenario1() {
 		
 		var endereco = new Endereco("rua santos", "santa paula", "12300777", "21", "bloco F", "Sao Paulo", "SP");
 		
 		Paciente paciente = new Paciente(null, "Ana", "ana@vold.med", "555555555", "477.528.639-74", endereco, true);
 		
-		DadosAtualizacaoPaciente dadosAtualizacaoPaciente = new DadosAtualizacaoPaciente(null, "Ana Francisca", "444444444", new DadosEndereco("rua santos", "santa paula", "12300777", "Sao Paulo", "SP", "bloco F", "21"));
+		DadosAtualizacaoPaciente dadosAtualizacaoPaciente = new DadosAtualizacaoPaciente(null, "Ana Francisca", "444444444", new DadosEndereco("rua indaia", "mooca", "12299777", "Rio de Janeiro", "RJ", "bloco L", "16"));
 		
 		paciente.atualizarInformacoes(dadosAtualizacaoPaciente);
 		
 		assertEquals("Ana Francisca", paciente.getNome());
 		assertEquals("444444444", paciente.getTelefone());
+		assertEquals("rua indaia", paciente.getEndereco().getLogradouro());
+		assertEquals("mooca", paciente.getEndereco().getBairro());
+		assertEquals("12299777", paciente.getEndereco().getCep());
+		assertEquals("Rio de Janeiro", paciente.getEndereco().getCidade());
+		assertEquals("RJ", paciente.getEndereco().getUf());
+		assertEquals("bloco L", paciente.getEndereco().getComplemento());
+		assertEquals("16", paciente.getEndereco().getNumero());
 		
+	}
+	
+	@Test
+	@DisplayName("Não deve alterar os dados do paciente, nome, telefone e endereco")
+	void testAtualizarInformacoesPaciente_cenario2() {
+		
+		var endereco = new Endereco("rua augusta", "tatuape", "09658410", "2687", "bloco K", "Bahia", "BA");
+		
+		Paciente paciente = new Paciente(null, "Silvia", "silvia@vold.med", "978524830", "433.599.449-36", endereco, true);
+		
+		DadosAtualizacaoPaciente dadosAtualizacaoPaciente = new DadosAtualizacaoPaciente(null, "Silvia", "978524830", new DadosEndereco("rua augusta", "tatuape", "09658410", "Bahia", "BA", "bloco K", "2687"));
+		
+		paciente.atualizarInformacoes(dadosAtualizacaoPaciente);
+		
+		assertEquals("Silvia", paciente.getNome());
+		assertEquals("978524830", paciente.getTelefone());
+		assertEquals("rua augusta", paciente.getEndereco().getLogradouro());
+		assertEquals("tatuape", paciente.getEndereco().getBairro());
+		assertEquals("09658410", paciente.getEndereco().getCep());
+		assertEquals("Bahia", paciente.getEndereco().getCidade());
+		assertEquals("BA", paciente.getEndereco().getUf());
+		assertEquals("bloco K", paciente.getEndereco().getComplemento());
+		assertEquals("2687", paciente.getEndereco().getNumero());
 		
 	}
 
