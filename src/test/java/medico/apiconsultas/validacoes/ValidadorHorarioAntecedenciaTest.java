633045d115e1e3d3052787b73231979b3c50cc72
Larissa Cruz <larissa.cruz@sofist.co>
2023-06-28 16:28:59 -0300

    testes dos validadores horario antecedencia e horario funcionamento clinica

---
+++ src/test/java/medico/apiconsultas/validacoes/ValidadorHorarioAntecedenciaTest.java
@@ -0,0 +1,42 @@
+package medico.apiconsultas.validacoes;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+
+import medico.apiconsultas.medico.*;
+import medico.apiconsultas.ValidacaoException;
+import medico.apiconsultas.consulta.DadosAgendamentoConsulta;
+
+
+public class ValidadorHorarioAntecedenciaTest {
+
+    @Test
+    @DisplayName("Teste agendar consulta com antecedencia suficiente = +30min")
+    public void testValidarComAntecedenciaSuficiente() {
+
+        ValidadorHorarioAntecedencia validador = new ValidadorHorarioAntecedencia();
+
+        LocalDateTime dataConsulta = LocalDateTime.now().plusMinutes(31);
+
+        DadosAgendamentoConsulta dados = new DadosAgendamentoConsulta(Long.valueOf(145), Long.valueOf(140), dataConsulta, Especialidade.CARDIOLOGIA);
+
+        Assertions.assertDoesNotThrow(() -> validador.validar(dados));
+
+    }
+
+    @Test 
+    @DisplayName("Teste agendar consulta sem antecedencia suficiente = -30min")
+    public void testValidarSemAntecedenciaSuficiente() {
+
+        ValidadorHorarioAntecedencia validador = new ValidadorHorarioAntecedencia();
+
+        LocalDateTime dataConsulta = LocalDateTime.now().plusMinutes(29);
+
+        DadosAgendamentoConsulta dados = new DadosAgendamentoConsulta(Long.valueOf(111), Long.valueOf(222), dataConsulta, Especialidade.CARDIOLOGIA);
+
+        Assertions.assertThrows(ValidacaoException.class, () -> validador.validar(dados));
+
+    }
+}
---
+++ src/test/java/medico/apiconsultas/validacoes/ValidadorHorarioFuncionamentoClinicaTest.java
@@ -0,0 +1,69 @@
+package medico.apiconsultas.validacoes;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjusters;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+
+import medico.apiconsultas.medico.*;
+import medico.apiconsultas.ValidacaoException;
+import medico.apiconsultas.consulta.DadosAgendamentoConsulta;
+import java.util.stream.Stream;
+
+public class ValidadorHorarioFuncionamentoClinicaTest {
+
+    @ParameterizedTest
+    @MethodSource("horariosDatasValidasConsultas")
+    @DisplayName("Teste agendar consulta em dia/horario validos de funcionamento da clinica")
+    public void testValidarHorarioFuncionamentoClinica(int hora, int minuto, DayOfWeek dia, Especialidade especialidade) {
+
+        ValidadorHorarioFuncionamentoClinica validador = new ValidadorHorarioFuncionamentoClinica();
+
+        LocalDateTime data = LocalDate.now().with(TemporalAdjusters.next(dia)).atTime(hora, minuto);
+
+        DadosAgendamentoConsulta dados = new DadosAgendamentoConsulta(Long.valueOf(845), Long.valueOf(445), data, especialidade);
+
+        Assertions.assertDoesNotThrow(() -> validador.validar(dados));
+
+    }
+
+        private static Stream<Arguments> horariosDatasValidasConsultas() {
+        return Stream.of(
+                Arguments.of(7, 0, DayOfWeek.MONDAY, Especialidade.CARDIOLOGIA),
+                Arguments.of(7, 1, DayOfWeek.TUESDAY, Especialidade.DERMATOLOGIA),
+                Arguments.of(10, 0, DayOfWeek.FRIDAY, Especialidade.CARDIOLOGIA),
+                Arguments.of(18, 58, DayOfWeek.WEDNESDAY, Especialidade.GINECOLOGIA),
+                Arguments.of(18, 59, DayOfWeek.THURSDAY, Especialidade.ORTOPEDIA));
+    }
+
+    @ParameterizedTest
+    @MethodSource("horariosDatasInvalidasConsultas")
+    @DisplayName("Teste agendar consulta em dia/horario invalidos de funcionamento da clinica")
+    public void testValidarHorarioInvalidoFuncionamentoClinica(int hora, int minuto, DayOfWeek dia, Especialidade especialidade) {
+
+        ValidadorHorarioFuncionamentoClinica validador = new ValidadorHorarioFuncionamentoClinica();
+
+        LocalDateTime data = LocalDate.now().with(TemporalAdjusters.next(dia)).atTime(hora, minuto);
+
+        DadosAgendamentoConsulta dados = new DadosAgendamentoConsulta(Long.valueOf(555), Long.valueOf(333), data, especialidade);
+
+        Assertions.assertThrows(ValidacaoException.class, () -> validador.validar(dados));
+    
+    }
+
+        private static Stream<Arguments> horariosDatasInvalidasConsultas() {
+        return Stream.of(
+                Arguments.of(6, 59, DayOfWeek.MONDAY, Especialidade.CARDIOLOGIA),
+                Arguments.of(5, 0, DayOfWeek.TUESDAY, Especialidade.DERMATOLOGIA),
+                Arguments.of(10, 0, DayOfWeek.SUNDAY, Especialidade.CARDIOLOGIA),
+                Arguments.of(20, 58, DayOfWeek.WEDNESDAY, Especialidade.GINECOLOGIA),
+                Arguments.of(21, 0, DayOfWeek.SATURDAY, Especialidade.ORTOPEDIA));
+    }
+
+}
